5c7d178e7c49a82c79315239c8f488c01e681dfa
chakkasailesh <chakkasailesh444@gmail.com>
2024-02-24 01:56:35 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Header } from './components'
+import './App.css'
 
 const App = () => {
-  return <div>App</div>
+  const [appData, setAppData] = useState({})
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.seekhoapp.com/api/v1.6/premium/premium-plans/?source=tab',
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_JWT_TOKEN}`,
+          },
+        }
+      )
+      .then((res) => setAppData(res.data))
+      .catch((err) => console.error(err))
+  }, [])
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="premium-summary">
+        <div>
+          <p>
+            10,000+
+            <br />
+            <span>SERIES</span>
+          </p>
+        </div>
+        <div>
+          NO
+          <br />
+          ADS
+        </div>
+        <div>
+          UNLIMITED
+          <br />
+          CONTENT
+        </div>
+      </div>
+      <h2>1 SAAL KE LIE UNLOCK KAREIN</h2>
+    </div>
+  )
+}
+
+export default Header
